e0196ef5e55eb022560de0cfd2e1cfc4872b3a41
Julius Türich <info@joonlabs.com>
2021-08-18 23:19:21 +0200

    add vue route to `public/vue/dist` folder

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use Curfle\FileSystem\FileSystem;
+use Curfle\Http\Request;
+use Curfle\Http\Response;
+use Curfle\Support\Exceptions\Http\NotFoundHttpException;
 use Curfle\Support\Facades\Route;
 
 /**
@@ -7,6 +11,29 @@ use Curfle\Support\Facades\Route;
  * will receive the web middleware stack. Now start creating something awesome!
  */
 
-Route::get('/', function () {
-    return "hello world!";
-});
+/**
+ * Serving route for the vue project. All requests are catched
+ * and redirected by this route. After trying to resolve the request
+ * it either returns the according file content, or throws a 404 HTTP
+ * dispatchable exception.
+ */
+Route::get('/{uri}', function (Request $request, Response $response, FileSystem $files) {
+
+    // determine uri
+    $uri = $request->input("uri");
+    if (in_array($uri, ["", "/"]))
+        $uri = "index.html";
+
+    // obtain real path in file system
+    $file = __DIR__ . "/../public/vue/dist/$uri";
+
+    // load the file if exists, or throw new 404 error
+    if ($files->exists($file)) {
+        $content = $files->get($file);
+        $response->setHeader("Content-Type", $files->contentType($file));
+        return $content;
+    } else {
+        throw new NotFoundHttpException("Not found", 404);
+    }
+
+})->where("uri", ".*");
