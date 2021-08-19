df3c3f47d34f2f290b40ab6fe0f83a1abe577beb
Julius Türich <info@joonlabs.com>
2021-08-20 00:13:26 +0200

    add database migrations

---
+++ database/migrations/20210819202500_CreateTableClient.php
@@ -0,0 +1,37 @@
+<?php
+
+use Curfle\Database\Migrations\Migration;
+use Curfle\Database\Schema\Blueprint;
+use Curfle\Support\Facades\Schema;
+
+class CreateTableClient extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("client", function (Blueprint $table) {
+            // fields
+            $table->id("id");
+            $table->string("name", 250);
+            $table->string("identifier", 50);
+            $table->float("map_latitude");
+            $table->float("map_longitude");
+            $table->int("map_zoom")->default(11);
+            $table->timestamp("created")->defaultCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("user");
+    }
+}
---
+++ database/migrations/20210819213148_CreateTableToken.php
@@ -0,0 +1,41 @@
+<?php
+
+use Curfle\Database\Migrations\Migration;
+use Curfle\Database\Schema\Blueprint;
+use Curfle\Database\Schema\ForeignKeyConstraint;
+use Curfle\Support\Facades\Schema;
+
+class CreateTableToken extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create("token", function (Blueprint $table) {
+            // fields
+            $table->id("id");
+            $table->int("user_id")->unsigned();
+            $table->string("token", 500);
+
+            // foreign key
+            $table->foreign("user_id")
+                ->references("id")
+                ->on("user")
+                ->onDelete(ForeignKeyConstraint::CASCADE)
+                ->onUpdate(ForeignKeyConstraint::CASCADE);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists("token");
+    }
+}
